aec7e598b882144aa20611be8204b33b27607a1e
Lyra Lee <leey2@rose-hulman.edu>
2023-05-15 15:24:59 -0400

    Finished input front-end

--- src/app.py
+++ src/app.py
@@ -3,15 +3,17 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
+print(openai.api_key)
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        feel_input = request.form["feel_input"]
         response = openai.Completion.create(
-            model="gpt-3.5-turbo",
-            prompt=generate_prompt(animal),
+            model="text-davinci-003",
+            prompt=generate_prompt(feel_input),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -20,7 +22,7 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
+def generate_prompt(feel_input):
     return """Suggest three names for an animal that is a superhero.
 
 Animal: Cat
@@ -29,5 +31,5 @@ Animal: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: {}
 Names:""".format(
-        animal.capitalize()
+        feel_input.capitalize()
     )
